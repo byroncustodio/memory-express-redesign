5b356626b23bf1f7aabd7aae216a799154d989e2
Byron Custodio <byron.custodio@gmail.com>
2019-05-28 13:51:24 -0700

    Add project files.

---
+++ MemoryExpress/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MemoryExpress.Startup))]
+namespace MemoryExpress
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
